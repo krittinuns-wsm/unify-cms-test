3d62df90d77154b2bb63822bdefd3edf69288637
Krittinun Sirodom <Krittinun.Sirodom@rockyou.com>
2018-11-07 17:45:26 +0700

    - connect to contentful - generate blogs from slug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,15 @@
+contentfulConfig = {
+  spaceId: "yben4ljlm0tf",
+  accessToken: "bf388c1eefa75d87aa089353535766a41b1783bc906c5c5756974455a7a5c549"
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  );
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -25,8 +37,13 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    },
+    'gatsby-transformer-remark',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};

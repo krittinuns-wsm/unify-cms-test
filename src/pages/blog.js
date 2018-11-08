fb5ec5e34278092b757cd9a707b5f64ace6d7a90
Krittinun Sirodom <Krittinun.Sirodom@rockyou.com>
2018-11-08 20:03:54 +0700

    use StaticQuery

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,45 +1,47 @@
 import React from 'react'
 //import Link from 'gatsby-link'
-import get from 'lodash/get'
+//import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import styles from './blog.module.css'
 import ArticlePreview from '../components/article-preview'
-import {graphql} from 'gatsby'
+import {StaticQuery, graphql} from 'gatsby'
 import Layout from '../components/layout'
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-
     return (
-      <Layout>
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <div className={styles.hero}>
-            Blog
-          </div>
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </Layout>
-    )
+      <StaticQuery
+        query={pageQuery}
+        render={data => (
+          <Layout>
+            <div style={{ background: '#fff' }}>
+              <Helmet title={data.site.siteMetadata.title} />
+              <div className={styles.hero}>
+                Blog
+              </div>
+              <div className="wrapper">
+                <h2 className="section-headline">Recent articles</h2>
+                <ul className="article-list">
+                  {data.allContentfulBlogPost.edges.map(({ node }) => {
+                    return (
+                      <li key={node.slug}>
+                        <ArticlePreview article={node} />
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            </div>
+          </Layout>
+        )}
+      />
+    ); 
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
-export const pageQuery = graphql`
+const pageQuery = graphql`
   query BlogIndexQuery {
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
@@ -61,5 +63,11 @@ export const pageQuery = graphql`
         }
       }
     }
+    site {
+      siteMetadata {
+        title
+      }
+    }
   }
 `
+
